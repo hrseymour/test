b52ac59646923cd0087d4e2ed213b23e4bedb4bc
Harlan Seymour <hrseymour@gmail.com>
2021-10-13 09:38:57 -0700

    Next step

--- test.py
+++ test.py
@@ -1,2 +1,7 @@
+import pandas as pd
+
 if __name__ == '__main__':
-    print('Hello world!')
+    x = pd.DataFrame({'values': list(range(1, 11))})
+    print(f'Hello world! {x}')
+    msg = 'Hello World'
+    print(msg)

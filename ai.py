c7804e87f2ea132101cac657e4eb44180f8b2eb8
Harlan Seymour <harlan@harlanseymour.com>
2023-10-15 14:18:46 -0700

    13 to 16

--- ai.py
+++ ai.py
@@ -1,6 +1,6 @@
 from langchain.chat_models import ChatOpenAI
 
 llm = ChatOpenAI()
-q = "Who was the 13th president of the US?"
+q = "Who was the 16th president of the US?"
 a = llm.predict(q)
 print(a)

1a7d0ccf54207fdec77394924a8790eaa4408e41
Harlan Seymour <harlan@harlanseymour.com>
2023-10-15 21:00:34 -0700

    Back to 13th

--- ai.py
+++ ai.py
@@ -1,6 +1,6 @@
 from langchain.chat_models import ChatOpenAI
 
 llm = ChatOpenAI()
-q = "Who was the 1st president of the US?"
+q = "Who was the 13th president of the US?"
 a = llm.predict(q)
 print(a)
